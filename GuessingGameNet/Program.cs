4d3b5c8e17a7c78cf2fc2d19eef1eadeac5b8eee
eruvalca <eruvalca@outlook.com>
2022-11-21 16:16:40 -0500

    Add project files.

---
+++ GuessingGameNet/Program.cs
@@ -0,0 +1,43 @@
+﻿
+int theAnswer;
+int playerGuess;
+string playerInput;
+
+Random r = new Random();
+theAnswer = r.Next(1, 21);
+
+do
+{
+    // get player input
+    Console.Write("Enter your guess (1-20): ");
+    playerInput = Console.ReadLine();
+
+    //attempt to convert the string to a number
+    if (int.TryParse(playerInput, out playerGuess))
+    {
+        if (playerGuess == theAnswer)
+        {
+            Console.WriteLine($"{theAnswer} was the number.  You Win!");
+            break;
+        }
+        else
+        {
+            if (playerGuess > theAnswer)
+            {
+                Console.WriteLine("Your guess was too High!");
+            }
+            else
+            {
+                Console.WriteLine("Your guess was too low!");
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("That wasn't a number!");
+    }
+
+} while (true);
+
+Console.WriteLine("Press any key to quit.");
+Console.ReadKey();
